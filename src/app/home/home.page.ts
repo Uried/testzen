6389f53c3c700367dc770d149b2394a3ff2994f4
uried.fongang <uried.fongang@sumelongenterprise.com>
2023-11-24 18:18:11 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  textToRead: string = "";
+  selectedLanguage: string = "";
+  selectedVoice: SpeechSynthesisVoice | null = null;
+  languages: { name: string, lang: string }[];
+  voices: SpeechSynthesisVoice[] = [];
 
-  constructor() {}
+  constructor() {
+    this.languages = [
+      { name: 'English', lang: 'en-US' },
+      { name: 'Français', lang: 'fr-FR' },
+      // Ajoutez d'autres langues selon vos besoins
+    ];
+  }
 
+  ionViewWillEnter() {
+    this.loadVoices();
+  }
+
+  loadVoices() {
+    this.voices = speechSynthesis.getVoices();
+  }
+
+  onLanguageChange() {
+    this.selectedVoice = null;
+    this.voices = speechSynthesis.getVoices().filter(voice => voice.lang === this.selectedLanguage);
+  }
+
+  speakText() {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = this.textToRead;
+    speech.lang = this.selectedLanguage;
+    speech.voice = this.selectedVoice;
+    speechSynthesis.speak(speech);
+  }
 }
+
